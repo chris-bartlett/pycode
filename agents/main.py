55f97fd337153a6c4421f2d8a61b949834892007
Chris Bartlett <chris-bartlett@outlook.com>
2025-02-24 10:59:46 -0500

    writing html to file from agent response

--- agents/main.py
+++ agents/main.py
@@ -5,23 +5,45 @@ from langchain.prompts import (
     MessagesPlaceholder
 )
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import SystemMessage
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
+from tools.report import write_report_tool
 
 
 load_dotenv()
 
 # agent_scratchpad is like a simplifed form of memory tracking conversation almost back and forth with tool requests and answers (original message, function call, and answer)
+#  for one agent_executor only. No memeory between different agent_executors
+#  can add memory and preserve the final ai message that is returned from the final function call along with the initial human message sent.
 chat = ChatOpenAI()
+
+tables = list_tables()
+
+# system message is to tell chat gpt to use describe tool not to just guess!
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content=(
+            "You are an AI that has access to a SQLite DB. \n"
+            f"The database has tables of : {tables}\n"
+            "Do not make any assumptions about what tables exist "
+            "or what columns exist. Instead, use the 'describe_tables' function"
+        )),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
-tools = [run_query_tool]
+# return messages as objects
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True
+)
+
+tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 # agent is a chain that knows how to do tools
 agent = OpenAIFunctionsAgent(
@@ -35,7 +57,14 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
-agent_executor("How many users are in the database?")
+# agent_executor("How many users are in the database?")
+#  agent_executor("How many users have provided a shipping address?")
+# agent_executor("Summarize the top 5 most popular products. Write the results to a report file.?")
+
+# # testing memory
+# agent_executor("How many orders were there? Write the results to a report file?")
+# agent_executor("Repeat the exact same process for users?")
--- agents/tools/sql.py
+++ agents/tools/sql.py
@@ -1,17 +1,48 @@
 import sqlite3
 from langchain.tools import Tool
+from pydantic.v1 import BaseModel
+from typing import List
 
 conn = sqlite3.connect("db.sqlite")
 
+def list_tables():
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = c.fetchall()
+    return "\n".join(row[0] for row in rows if row[0] is not None)
 
+# if there was an error with the query return the errror instead
 def run_sqlite_query(query):
     c = conn.cursor()
-    c.execute(query)
-    return c.fetchall()
+    try:
+        c.execute(query)
+        return c.fetchall()
+    except sqlite3.OperationalError as err:
+        return f"The following error occured: {str(err)}"
 
+class RunQueryArgsSchema(BaseModel):
+    query: str
 
+# tell chat gpt to run this tool and it needs to call the arguemnt query
 run_query_tool = Tool.from_function(
     name="run_sqlite_query",
     description="Run a sqlite query.",
-    func=run_sqlite_query
+    func=run_sqlite_query,
+    args_schema=RunQueryArgsSchema
+)
+
+def describe_tables(table_names):
+    c = conn.cursor()
+    tables = ', '.join("'" + table + "'" for table in table_names)
+    rows = c.execute(f"SELECT sql FROM sqlite_master WHERE type='table' AND name IN ({tables});")
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+class DescribeTablesArgsSchema(BaseModel):
+    table_names: List[str]
+
+describe_tables_tool = Tool.from_function(
+    name="describe_tables",
+    description="Given a list of table names, returns the schema of those tables.",
+    func=describe_tables,
+    args_schema=DescribeTablesArgsSchema
 )
