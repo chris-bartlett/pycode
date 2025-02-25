3d8dfd4fb9a7a2418f08ef687c9be5615ec767c6
Chris Bartlett <chris-bartlett@outlook.com>
2025-02-25 16:51:48 -0500

    updating chat section

--- pdf/app/chat/chat.py
+++ pdf/app/chat/chat.py
@@ -1,16 +1,17 @@
 from app.chat.models import ChatArgs
-
+from app.chat.vector_stores.pinecone import build_retriever
+from langchain.chains import ConversationalRetrievalChain
+from app.chat.llm.chatopenai import build_llm
+from app.chat.memories.sql_memories import build_memory
 
 def build_chat(chat_args: ChatArgs):
-    """
-    :param chat_args: ChatArgs object containing
-        conversation_id, pdf_id, metadata, and streaming flag.
+    retriever = build_retriever(chat_args)
+    llm = build_llm(chat_args)
+    memory = build_memory(chat_args)
 
-    :return: A chain
-
-    Example Usage:
-
-        chain = build_chat(chat_args)
-    """
-
-    pass
+    return ConversationalRetrievalChain.from_llm(
+        retriever=retriever,
+        llm=llm,
+        memory=memory
+    )
+    
